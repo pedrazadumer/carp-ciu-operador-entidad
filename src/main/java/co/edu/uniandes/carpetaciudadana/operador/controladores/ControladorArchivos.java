38d01baf589d32ab05168a1615ab1d6babab7b6b
pedrazadumer <pedrazadumer@gmail.com>
2021-05-31 20:44:50 -0500

    Mejoras cambiando de array a List.

--- src/main/java/co/edu/uniandes/carpetaciudadana/operador/controladores/ControladorArchivos.java
+++ src/main/java/co/edu/uniandes/carpetaciudadana/operador/controladores/ControladorArchivos.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -19,7 +18,7 @@ public class ControladorArchivos {
             "\n===============================================================\n";
 
     @PostMapping("operador/archivos/{nit}")
-    public void loadFiles(@RequestParam("archivos") MultipartFile[] archivos,
+    public void loadFiles(@RequestParam("archivos") List<MultipartFile> archivos,
                                        @PathVariable String nit) {
         if (isNotEmpty(archivos)) {
             System.out.printf("\nArchivos recibidos para la entidad con NIT [%s]:\n\n", nit);
@@ -30,21 +29,21 @@ public class ControladorArchivos {
         }
     }
 
-    private boolean isNotEmpty(MultipartFile[] archivos) {
-        return Arrays.stream(archivos)
+    private boolean isNotEmpty(List<MultipartFile> archivos) {
+        return archivos.stream()
                 .anyMatch(f -> !StringUtils.isEmpty(f.getOriginalFilename()));
     }
 
-    private static void printFiles(MultipartFile[] archivos) {
+    private static void printFiles(List<MultipartFile> archivos) {
         System.out.printf("Nombre del Archivo%s", SEPARADOR);
 
-        for (int i = 0; i < archivos.length; i++) {
-            MultipartFile archivo = archivos[i];
+        for (int i = 0; i < archivos.size(); i++) {
+            MultipartFile archivo = archivos.get(i);
             System.out.printf("=> %s", archivo.getOriginalFilename());
-            if (i < archivos.length - 1) System.out.println();
+            if (i < archivos.size() - 1) System.out.println();
         }
 
         System.out.print(SEPARADOR);
-        System.out.printf("Total %s archivo(s).\n\n", archivos.length);
+        System.out.printf("Total %s archivo(s).\n\n", archivos.size());
     }
 }
